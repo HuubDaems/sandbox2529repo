503b0fe08ff7854a87b4c3513599acd531f3f2d3
Huub Daems <hdaems@redhat.com>
2024-09-27 17:39:54 +0200

    pac next step 2

--- pipelines-as-code-secret.sh
+++ pipelines-as-code-secret.sh
@@ -3,7 +3,7 @@
 set -x
 
 oc -n openshift-pipelines create secret generic pipelines-as-code-secret \
-        --from-literal github-private-key="$(cat ../priv_key)" \
+        --from-literal github-private-key="$(cat ../sandbox2529.2024-09-27.private-key.pem)" \
         --from-literal github-application-id="1006839" \
         --from-literal webhook.secret="$(cat ../secret.txt)"
 
