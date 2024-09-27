3d38de1dab12f7d5c718660bc3d57807f60a65d3
Huub Daems <hdaems@redhat.com>
2024-09-27 17:02:57 +0200

    trigger and listener

--- pipelines/apply.sh
+++ pipelines/apply.sh
@@ -9,5 +9,11 @@ oc apply -f 'tasks/*yaml' -n $PROJECT
 
 oc apply -f 'maven-pipeline.yaml' -n $PROJECT
 
-oc create -f 'maven-pipeline-run.yaml' -n $PROJECT
+# oc create -f 'maven-pipeline-run.yaml' -n $PROJECT
+
+oc apply -f 'trigger-template-maven-pipeline.yaml' -n $PROJECT
+
+oc apply -f 'trigger-binding-maven-pipeline.yaml' -n $PROJECT
+
+oc apply -f 'event-listener-maven-pipeline.yaml' -n $PROJECT
 
